d70c153607da4aaafacaa3ca02308918810b6084
Lucas Nunes <lucas.parreira.nunes@gmail.com>
2023-03-04 18:55:32 -0300

    defining popup content

--- popup.tsx
+++ popup.tsx
@@ -6,20 +6,31 @@ function IndexPopup() {
   return (
     <div
       style={{
+        minWidth: 300,
         display: "flex",
         flexDirection: "column",
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+      <h2
+        style={{
+          fontSize: 20,
+          padding: 0,
+          margin: 0
+        }}>
+        Twitch Activity Feed Checker ☑
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
+      <p
+        style={{
+          fontSize: 18
+        }}>
+        Tired of not knowing which sub/follower you already thanked? Now you
+        keep it checked
+      </p>
+      <a
+        href="https://github.com/lucascnunes"
+        target="_blank"
+        style={{ fontSize: 16, textDecoration: "none", color: "#643cbb" }}>
+        Created with ♥️ by Lucas from 🇧🇷 Brazil
       </a>
     </div>
   )
